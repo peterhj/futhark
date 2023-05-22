a517b914163039df8551a1b145eb535eb40ba164
Peter Jin <>
2023-05-22 12:22:31 -0400

    Include the right header (C, not C++).

--- src/Futhark/CodeGen/Backends/CCUDA.hs
+++ src/Futhark/CodeGen/Backends/CCUDA.hs
@@ -80,7 +80,7 @@ compileProg version prog = do
     cuda_includes =
       [untrimming|
        #include <cuda.h>
-       #include <cuda_runtime.h>
+       #include <cuda_runtime_api.h>
        #include <nvrtc.h>
       |]
 
